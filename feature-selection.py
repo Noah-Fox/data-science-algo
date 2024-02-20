3fec4b5fce620a614ed6b872ec310c11423be194
Noah Fox <89417603+Noah-Fox@users.noreply.github.com>
2024-02-20 14:57:11 -0500

    Feature selection assignment

---
+++ feature-selection.py
@@ -0,0 +1,117 @@
+import numpy
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import hist1_analysis as h1_mod
+import seaborn as sns
+import random
+
+def main():
+    #input data file
+    dataFile = 'GSE64881_segmentation_at_30000bp.passqc.multibam.txt'
+    df = pd.read_csv(dataFile,sep='\t')
+
+    #open output file
+    outputFile = open("reports/feature-selection-report.md", 'w')
+    outputFile.write('# Feature Selection Report\n')
+
+    #columns denote an NP, rows denote a window
+    windowDetectionsDf = df.iloc[:,3:]
+
+    #columns denote chromosome name and start and stop position, rows denote a window
+    windowValuesDf = df.iloc[:,0:3]
+
+    #extract Hist1 windows and NPs
+    hist1Windows = h1_mod.findHist1Windows(windowValuesDf)
+    hist1WindowDetectionsDf = windowDetectionsDf.iloc[hist1Windows,:]
+    hist1NpSums = h1_mod.windowsPerNP(hist1WindowDetectionsDf)
+    hist1NPs = list(hist1NpSums.index)
+
+    #construct a matrix of Jaccard Indices for each pair of NPs
+    jaccardData = [[h1_mod.normalizedJaccard(a,b,hist1WindowDetectionsDf) for a in hist1NPs] for b in hist1NPs]
+    npJaccards = pd.DataFrame(data=jaccardData, index=hist1NPs, columns=hist1NPs)
+
+    #Run K-medoids clustering
+    npCombos = []
+    for x in range(0,len(hist1NPs)):
+        for y in range(x+1,len(hist1NPs)):
+            for z in range(y+1,len(hist1NPs)):
+                npCombos.append([hist1NPs[x],hist1NPs[y],hist1NPs[z]])
+    
+    selectCombos = random.sample(npCombos,100)
+    clusteringScores = []
+    maxScore = 0
+    optimalClustering = []
+
+    for i in selectCombos:
+        print(i,end=' ')
+        clusters, clusterMedoids = runKMedoidsClustering(i,hist1NPs,npJaccards)
+        clusteringScore = assignClusteringScores(clusters,clusterMedoids,npJaccards)
+        clusteringScores.append(clusteringScore)
+        print(clusteringScore['similarityAvg'],len(clusteringScores))
+        if clusteringScore['similarityAvg'] > maxScore:
+            maxScore = clusteringScore['similarityAvg']
+            optimalClustering = clusters
+    
+    plt.figure()
+    plt.title('Intra-cluster similarity averages')
+    plt.scatter(range(0,len(selectCombos)),list(map(lambda x: x['similarityAvg'], clusteringScores)))
+    saveToFile = 'charts/feature-selection/similarity-averages.png'
+    plt.savefig(saveToFile)
+    outputFile.write('![Intra-cluster similarity averages](../' + saveToFile + ')\n\n')
+
+    outputFile.write('Maximum similarity average found: ' + str(maxScore) + '\n\n')
+
+
+    for i in range(0,3):
+        plt.figure()
+        sns.heatmap(hist1WindowDetectionsDf.loc[:,clusters[i]],cmap='Blues')
+        plt.title('Cluster ' + str(i))
+        saveToFile = 'charts/feature-selection/cluster-' + str(i) + '-heatmap.png'
+        plt.savefig(saveToFile)
+        outputFile.write('![Cluster ' + str(i) + '](../' + saveToFile + ')\n\n')
+
+
+    # for i in range(0,100):
+    #     clusters,clusterMedoids = runKMedoidsClustering(['F10B5', 'F11B1', 'F3F1'],hist1NPs,npJaccards)
+    #     print([len(x) for x in clusters])
+    #     print(clusterMedoids,'\n')
+
+
+    return 
+
+
+def assignClusteringScores(clusters,clusterMedoids,npJaccards):
+    npAmount = sum([len(c) for c in clusters])
+    scores = {
+        'similaritySum': sum([sum([npJaccards[np][med] for np in c]) for c,med in zip(clusters,clusterMedoids)]),
+        'similarityAvg': sum([sum([npJaccards[np][med] for np in c]) for c,med in zip(clusters,clusterMedoids)])/npAmount,
+        'distanceSum': sum([sum([1-npJaccards[np][med] for np in c]) for c,med in zip(clusters,clusterMedoids)]),
+        'distanceAvg': sum([sum([1-npJaccards[np][med] for np in c]) for c,med in zip(clusters,clusterMedoids)])/npAmount
+    }
+    return scores
+
+
+#performs K-Medoids clustering on values in hist1NPs, with similarities denoted in npJaccards, starting with clusterMedoids medoids
+#returns clusters and clusterMedoids
+def runKMedoidsClustering(clusterMedoids, hist1NPs, npJaccards):
+    clusterPreferences = [random.randint(0,3) for x in hist1NPs]
+        #An arbitrary preference of which cluster to be assigned to in case of a tie
+    clusterAssignments = h1_mod.assignKMeansCluster(hist1NPs, clusterMedoids, npJaccards, clusterPreferences)
+    clusters = h1_mod.arrangeClusters(clusterAssignments, 3)
+    running = True
+    iterations = 1
+    while (running):
+        clusterMedoids = [h1_mod.findClusterMedoid(c,npJaccards) for c in clusters]
+        nextAssignments = h1_mod.assignKMeansCluster(hist1NPs, clusterMedoids, npJaccards, clusterPreferences)
+        nextClusters = h1_mod.arrangeClusters(clusterAssignments,3)
+        if (nextAssignments == clusterAssignments):
+            running = False
+        clusterAssignments = nextAssignments
+        clusters = nextClusters
+        iterations += 1
+
+    return clusters, clusterMedoids
+
+if __name__ == "__main__":
+    main()
--- hist1_analysis.py
+++ hist1_analysis.py
@@ -1,3 +1,11 @@
+import numpy
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import hist1_analysis as h1_mod
+import seaborn as sns
+import random
+
 #returns a list of numbers of chr13 windows that are between 21.7 and 24.1 Mb
 def findHist1Windows(windowValuesDf):
     chromType = "chr13"
@@ -35,3 +43,42 @@ def findWindowCompactions(windowDetectionsDf):
     for index,key in enumerate(windowSumsSorted.keys()):
         windowSumsSorted[key] = math.floor(index / (len(windowSumsSorted)/10)) + 1
     return windowSumsSorted
+
+#Given lists of NPs in each cluster and the medoid of each cluster, return the sum of distances from every NP to its medoid
+def assessClusteringQuality(clusters, medoids, npJaccards):
+    return sum([sum([(1-npJaccards[np][med]) for np in c]) for (c, med) in zip(clusters,medoids)])
+
+#Assign each element of hist1NPs to a cluster with an element of clusterMedoids at its center
+#Each element of the returned list denotes an NP, containing [cluster, np]
+def assignKMeansCluster(hist1NPs, clusterMedoids, npJaccards, preferences):
+    return [[assignCluster(clusterMedoids,n,npJaccards,pref),n] for (n,pref) in zip(hist1NPs,preferences)]
+
+#Given an NP and the NPs at the center of each cluster, determine which cluster is closest
+def assignCluster(clusterMedoids, np, npJaccards, preference):
+    clusterSimilarities = [npJaccards[np][c] for c in clusterMedoids]
+    return randMax(clusterSimilarities,randOffset=preference)
+
+#Given the cluster assigned to each element, return k lists of NPs in each cluster
+def arrangeClusters(clusterAssignments, k):
+    return [list(map(lambda x : x[1], filter(lambda x: x[0] == i, clusterAssignments))) for i in range(0,k)]
+
+#Given a list of NPs in a cluster, find the medoid - the NP with the maximum average similarity to all other NPs in the cluster
+def findClusterMedoid(cluster, npJaccards):
+    avgSimilarities = [sum([(npJaccards[n][cn]) for cn in cluster])/len(cluster) for n in cluster]
+    return cluster[avgSimilarities.index(max(avgSimilarities))]
+
+#Given a list of numbers, return the index of the maximum number
+#If there are multiple maximums, choose one randomly
+def randMax(dataList,randOffset=-1):
+    maxVal = max(dataList)
+    doubledList = dataList + dataList
+    if (randOffset == -1):
+        randOffset = random.randint(0,len(dataList)-1)
+    return doubledList.index(maxVal,randOffset) % len(dataList)
+
+#Given two NPs, find their Normalized Jaccard Index - Jn(A,B) = (A INTERSECTION B) / min(|A|, |B|)
+def normalizedJaccard(npA, npB, hist1WindowDetectionsDf):
+    A = (hist1WindowDetectionsDf[npA] == 1)
+    B = (hist1WindowDetectionsDf[npB] == 1)
+    if (A | B).sum() == 0: return 0
+    return (A & B).sum() / min(A.sum(), B.sum())
