e5112ad61cf264161c7da9c1a9a8de2100be2508
Noah Fox <89417603+Noah-Fox@users.noreply.github.com>
2024-03-21 14:07:33 -0400

    Add explanations

--- co-segregation-1.py
+++ co-segregation-1.py
@@ -51,6 +51,35 @@ def main():
     plt.savefig('charts/co-segregation-1/linkage-table.png')
     outputFile.write('![Linkage Table heat map](../charts/co-segregation-1/linkage-table.png)\n\n')
 
+    #write an explanation in the report
+    outputFile.write('The detection frequency f(A) of a window A is the number of NPs which detect A, divided by the total number of NPs\n\n')
+    outputFile.write('The co-segregation f(A,B) of two windows A and B is the number of NPs which detect both windows, divided by the total number of NPs\n\n')
+    outputFile.write('The linkage disequilibrium D is equal to f(A,B)-f(A)f(B)\n\n')
+    outputFile.write('The theoretical maximum of D, D-max, can be found as:\n\n')
+    outputFile.write('\t min(f(A)f(B), (1-f(A))(1-f(B))) when D < 0\n\n')
+    outputFile.write('\t min(f(B)(1-f(A)), f(A)(1-f(B))) when D > 0\n\n')
+    outputFile.write('\t 1 when D = 0\n\n')
+    outputFile.write('The normalized linkage disequilibrium, D-norm, is equal to D/D-max\n\n')
+    outputFile.write('D-norm functions such that, when A and B have the maximum amount of NPs in common, it is equal to 1, ')
+    outputFile.write('and when they have no NPs in common, it is -1.\n\n')
+
+    #create a clarified heat map of the linkage table
+    clarLinkageTable = linkageTable 
+    for i in indices:
+        for x in indices:
+            val = clarLinkageTable.loc[i,x]
+            if val < 0 and val != -1:
+                val = -0.5
+            elif val > 0 and val != 1:
+                val = 0.5
+            clarLinkageTable.loc[i,x] = val
+    plt.figure()
+    sns.heatmap(clarLinkageTable,cmap='Blues')
+    plt.title('Clarified Linkage Table')
+    plt.savefig('charts/co-segregation-1/clarified-linkage-table.png')
+    outputFile.write('![Linkage Table heat map](../charts/co-segregation-1/clarified-linkage-table.png)\n\n') 
+    outputFile.write('Linkage table with all non-integer values rounded to -0.5 and 0.5, to highlight outlying values of -1, 0, and 1\n\n')
+
     return 
 
 #number of NPs in which a window is detected, divided by total numbers of NPs
@@ -76,6 +105,13 @@ def normalizedLinkage(windowA, windowB):
     elif D > 0:
         return D / min(fB * (1-fA), fA * (1-fB))
     return 0
+
+# D-norm will be 1 when two windows have the maximum amount of shared NPs, and -1 when they have the minimum
+
+# Notes on normalized linkage being divided by zero:
+#   - As D approaches 0, D-norm approaches 0 (if D=0, D-norm should be 0)
+#   - As D-max approaches 0, D-norm approaches 1 or -1 (D-max will be 0 if a window is present in all or none of the NPs. In this
+#   case, D-norm should be 1 or -1)
 
 
 if __name__ == "__main__":
